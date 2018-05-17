32e290358ca75edf19115cbeae55a0f58ac177a1
Chi-Hsin <jack1234552000@yahoo.com.tw>
2018-05-17 18:29:48 +0800

    Site updated: 2018-05-17 18:26:16

--- js/myjs.js
+++ js/myjs.js
@@ -4,4 +4,22 @@ $(function(){
 		$(this).css("display","inline-block").css("color","blue")
 		.attr("href","#"+text);
 	})
+	$("h1").hover(function(){
+		var skill_name = $(this).text();
+		function aaa()
+		{
+			var obj = document.createElement("span");
+			obj.style.background = "rgb(51,122,183)";
+			obj.style.color = "#fff";
+			obj.style.width = "300px";
+			obj.style.height = "25px";
+			obj.style.fontSize = "15px";
+			obj.style.marginLeft = "10px";
+			obj.style.textAlign = "center";
+			obj.textContent = "點擊查看擁有"+skill_name+"的卡片";
+			return obj;
+		}
+		$(this).append(aaa());
+		},function(){$(this).find("span").remove()}
+	)
 })
